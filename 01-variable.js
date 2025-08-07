d827436876edb63479a3082e5e37a9a702231183
Zunair Saleem <zunairsaleem06@gmail.com>
2025-08-07 16:52:03 +0500

    new changes

---
+++ 01-variable.js
@@ -0,0 +1,8 @@
+const first = "018" //Not 
+let second = "mine@gmail.com" //change work within the scope
+var third = "12344" //globally available anywhere will be change
+fouth = "98w78wyw" //not prefered javascript define itself variable
+
+// console.log(first)
+
+console.table([first,second,third,fouth])
